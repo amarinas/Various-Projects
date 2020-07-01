7172a51a83c2de88d810539602284e2fd50e45d7
ali <ali.marinas@icloud.com>
2020-07-01 01:23:06 -0700

    added useState hooks

--- speed-typing-game/src/App.js
+++ speed-typing-game/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useState}from 'react';
 
 import './App.css';
 
 function App() {
+  const [text, setText] = useState("")
+
+
+  function handleChange(e){
+      const {value} = e.target
+      setText(value)
+  }
+
+console.log(text)
+
   return (
     <div className="App">
-        <h1>Speed Typing Game</h1>
-        <div>
-          <textarea />
-        </div>
-        <input />
+        <h1>How fast can you type?</h1>
+        <textarea
+            onChange={handleChange}
+            value={text}
+        />
         <h4>Time Remaining</h4>
         <button>Start Game</button>
         <h1>Word Count Per minute</h1>
