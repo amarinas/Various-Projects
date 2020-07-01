ca81642ce52d9af6d0b36cbbcc1e183af3e02184
ali <ali.marinas@icloud.com>
2020-07-01 01:01:22 -0700

    first commit

---
+++ speed-typing-game/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Speed Typing Game</h1>
+        <div>
+          <textarea />
+        </div>
+        <input />
+        <h4>Time Remaining</h4>
+        <button>Start Game</button>
+        <h1>Word Count Per minute</h1>
+    </div>
+  );
+}
+
+export default App;
